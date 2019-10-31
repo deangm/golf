abf142e360d04c5ce40412ae871180e55c1c3480
mikey <michael.d.deangelo@Gmail.com>
2019-10-30 20:14:02 -0600

    lods in data based on selection

--- js/action.js
+++ js/action.js
@@ -1,13 +1,98 @@
 let courses = request.get("https://golf-courses-api.herokuapp.com/courses")
-let courseCollection = new CourseCollection();
 
 courses.then(val => {
+    let html = "";
+
     val.courses.forEach(element => {
-        courseCollection.addCourse(element);
+        console.log(element);
+        html += `
+                <div class = "card">
+                    <div class = "courseName">
+                        ${element.name}
+                    </div>
+                    <div class = "courseImage">
+                        <img src = ${element.image}>
+                    </div>
+                    <div class = "courseBtn">
+                        <button onclick = "showTeeBoxes(${element.id})"> Choose This Course </button>
+                    </div>
+                    <div id = "${element.id}">
+                    </div>
+                </div>`;
+        $("#courseCardCont").html(html);
     });
 });
 
 
-courses.then(val =>{
-    console.log(val);
-});
+function showTeeBoxes(id) {
+    let courseSelection = request.get(`https://golf-courses-api.herokuapp.com/courses/${id}`);
+
+    courseSelection.then(val => {
+        let numHoles = val.data.holes;
+        numHoles[0].teeBoxes.forEach(tee => {
+            $(`#${id}`).append(`
+                <div onclick = "initializeCard(${id}, '${tee.teeType}')">
+                    ${tee.teeType}
+                </div>
+            `)
+        });
+
+    });
+};
+
+function initializeCard(id, tee) {
+    let courseSelection = request.get(`https://golf-courses-api.herokuapp.com/courses/${id}`);
+    courseSelection.then(val => {
+
+        let holes = val.data.holes;
+
+        $("#cardCont").html("");
+
+        $("#cardCont").append(`
+            <div class = "col" id = "headings">
+                <div>Hole:</div>
+                <div>Yards</div>
+                <div>Par</div>
+                <div>HCP</div>
+            </div>
+         `);
+        for (let i = 0; i < holes.length; i++) {
+
+            let thing = holes[i].teeBoxes.find(val => {
+                return val.teeType == tee;
+            });
+
+            let yards = thing.yards;
+            let par = thing.par;
+            let hcp = thing.hcp;
+            let hole = holes[i].hole;
+
+            $("#cardCont").append(`
+                <div class = "col" id = "col${i}">
+                    <div>Hole ${hole}</div>
+                    <div>${yards}</div>
+                    <div>${par}</div>
+                    <div>${hcp}</div>
+                </div>
+            `);
+        };
+        addPlayersToCard(holes.length, 3);
+    })
+}
+
+function addPlayersToCard(holes, players) {
+    for (let player = 1; player <= players; player++) {
+        $(`#headings`).append(`
+        <div id = "${player}">
+            Player:${player}
+        </div>
+    `)
+        for (let hole = 0; hole < holes; hole++) {
+            $(`#col${hole}`).append(`
+                <div id = "${player}${hole+1}">
+                    ${player}:${hole+1}
+                </div>
+            `)
+        }
+    }
+}
